b539fd76b4cf03e7fb62dfdab2a3130f4f35886c
swathi <swathi.yernagula@gmail.com>
2025-12-01 06:01:04 -0500

    functions - grocery sum

---
+++ calculateArea/groceryTracker.js
@@ -0,0 +1,8 @@
+let grocery1, grocery2, grocery3;
+function calculateGrocerySum() {
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+    let sum = grocery1 + grocery2 + grocery3;
+    document.getElementById('result').innerText = `The total amount is: ${sum}`;
+}
